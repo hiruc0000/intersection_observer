b60347ea3f92acfa658ea0010343904d90186e9b
hiruc0000 <81366454+hiruc0000@users.noreply.github.com>
2021-04-17 22:00:44 +0900

    ビューポートに入った際section-titleのフェードイン化

--- observer.js
+++ observer.js
@@ -1,35 +1,57 @@
 (() => {
   const sections = document.querySelectorAll('section');
+  const titles = document.querySelectorAll('.section-title');
 
-  const options = {
-    root: null,
-    rootMargin: '-50% 0px',
-    threshold: 0
-  }
+  // const options = {
+  //   root: null,
+  //   rootMargin: '-50% 0px',
+  //   threshold: 0
+  // }
 
-  const observer = new IntersectionObserver(callback, options);
+  const observer = new IntersectionObserver(callback, options = {rootMargin: '-50% 0px'});
+  const observer2 = new IntersectionObserver(callback2, options = {rootMargin: '0px 0px -20%'});
 
   sections.forEach((section) => {
     observer.observe(section);
   });
+
+  titles.forEach((title) => {
+    observer2.observe(title);
+  });
 })();
 
+
+//  ヘッダーナビへの処理
 function callback(entries, observer) {
   entries.forEach((entry) => {
     if(entry.isIntersecting){
-      selectElement(entry.target);
-      //observer.unobserve(entry.target); //監視を一度で辞める場合に必要
+      selectIndex(entry.target);
+      // observer.unobserve(entry.target); //監視を一度で辞める場合に必要
     }
   });
 }
 
-function selectElement(target) {
-  const activeIndex = document.querySelector("#index-list .select");
+function selectIndex(target) {
+  const activeIndex = document.querySelector('#index-list .select');
 
   if (activeIndex !== null) {
-    activeIndex.classList.remove("select");
+    activeIndex.classList.remove('select');
   }
 
   const newActiveIndex = document.querySelector(`a[href="#${target.id}"]`);
-  newActiveIndex.classList.add("select");
+  newActiveIndex.classList.add('select');
+}
+
+//  セクションタイトルへの処理
+function callback2(entries, observer) {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting){
+      fadein(entry.target);
+      observer.unobserve(entry.target); //監視を一度で辞める場合に必要
+    }
+  });
+}
+
+function fadein(target) {
+  target.classList.add('fadein');
 }
